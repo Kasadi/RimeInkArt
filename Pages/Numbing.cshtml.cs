a470dd458bcd88efe74b1368c1c710d87070205a
Kasó Ádám <kaso.adam@gmail.com>
2022-10-17 01:17:28 +0200

    2022.10.16 Géptakarítás után

---
+++ Pages/Numbing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RimeInkArt.Pages
+{
+    public class NumbingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
