923a8dfe62babb330dd0b93b1e7556c701e2043f
Kasó Ádám <kaso.adam@gmail.com>
2022-10-18 23:11:30 +0200

    2022.10.18. - Finished in my opninion but if the customer wants changes then it is not.

--- Pages/Galery.cshtml.cs
+++ Pages/Galery.cshtml.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.Hosting.Internal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Policy;
 
 namespace RimeInkArt.Pages
 {
     public class GaleryModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+
+
+        public IEnumerable<string> Images { get; set; }
+
+        public void OnGet() => Images = Directory.GetFiles(@"wwwroot/images/portfolio_kepek/");
     }
 }
